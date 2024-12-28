1f9b9aff558462182b96c32334162bdc5065be7a
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-28 10:00:47 -0300

    Buuble Sort Update

---
+++ Library Bubble Sort On One Dimensional Array/BubbleSort01.cs
@@ -0,0 +1,23 @@
+﻿// Link to the problem: https://leetcode.com/problems/move-zeroes/
+
+namespace LibraryBubbleSort
+{
+    public class BubbleSort01
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            for (int i = 0; i < nums.Length -1; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] is 0)
+                    {
+                        int aux = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = aux;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ View Bubble Sort On One Dimensional Array/Program.cs
@@ -0,0 +1,10 @@
+﻿using LibraryBubbleSort;
+
+
+// Bubble Sort 01
+int[] nums = [0, 1, 0, 3, 12];
+BubbleSort01.MoveZeroes(nums);
+
+for (int i = 0; i < nums.Length; i++)
+    Console.Write($" {nums[i].ToString()}");
+
